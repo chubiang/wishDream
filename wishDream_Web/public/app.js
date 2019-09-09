56df22e9b33032e0c460e92f37de7dc0dac04197
chibiang <jiyeeon1208@gmail.com>
2019-09-09 18:32:26 +0900

    -

--- wishDream_Web/public/app.js
+++ wishDream_Web/public/app.js
@@ -3,7 +3,7 @@ import React, { Component, Fragment } from 'react';
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import pink from '@material-ui/core/colors/pink';
 import red from '@material-ui/core/colors/red';
-import BaseLayout from "Component/baseLayout";
+import BaseLayout from "Component/Layout/BaseLayout";
 import "styles/app.scss";
 import "styles/material_font_roboto.css";
 import "styles/material_icon.css";
